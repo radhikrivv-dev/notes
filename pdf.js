2339304d8b8c6b6de986d7b66eeefc92a3125134
radhikrivv-dev <radhikrivv@gmail.com>
2026-08-01 10:34:09 +0545

    Add files via upload

---
+++ pdf.js
@@ -0,0 +1,68 @@
+// PDF Viewer Script
+
+const urlParams = new URLSearchParams(window.location.search);
+const pdfFile = urlParams.get("file");
+
+const pdfUrl = "pdf/" + pdfFile;
+
+let pdfDoc = null;
+let pageNum = 1;
+
+const canvas = document.getElementById("pdfCanvas");
+const ctx = canvas.getContext("2d");
+
+function renderPage(num){
+
+pdfDoc.getPage(num).then(function(page){
+
+let viewport = page.getViewport({scale:1.5});
+
+canvas.height = viewport.height;
+canvas.width = viewport.width;
+
+let renderContext = {
+canvasContext:ctx,
+viewport:viewport
+};
+
+page.render(renderContext);
+
+document.getElementById("pageNumber").innerHTML =
+"Page " + num + " / " + pdfDoc.numPages;
+
+});
+
+}
+
+
+pdfjsLib.getDocument(pdfUrl).promise.then(function(pdf){
+
+pdfDoc = pdf;
+
+renderPage(pageNum);
+
+});
+
+
+function nextPage(){
+
+if(pageNum < pdfDoc.numPages){
+
+pageNum++;
+renderPage(pageNum);
+
+}
+
+}
+
+
+function previousPage(){
+
+if(pageNum > 1){
+
+pageNum--;
+renderPage(pageNum);
+
+}
+
+}
